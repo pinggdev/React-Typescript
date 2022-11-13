98fe4105f3a2aa5152139fe80392dc8bd458dc3f
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2022-11-13 23:07:42 +0700

    useState Type Assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,23 +6,26 @@ type AuthUser = {
 };
 
 export const User = () => {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  //   const [user, setUser] = useState<AuthUser | null>(null);
   // setUser telah didefiniskan di awal sebagai null, ketika ingin meninsialisasi lagi kedalam setUser, maka buat sebuah <> didalam useState, sehingga nilai dari useState tersebut menjadi dua, yaitu AuhtUser dan null, initial valuenya null, tetapi kedepan bisa menjadi AuthUser.
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
   const handleLogin = () => {
     setUser({
       name: "Kevin",
       email: "kevin@example.com",
     });
   };
-  const handleLogout = () => {
-    setUser(null);
-  };
+  //   const handleLogout = () => {
+  //     setUser(null);
+  //   };
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>User name is {user?.name}</div>
-      <div>User email is {user?.email}</div>
+      {/* <button onClick={handleLogout}>Logout</button> */}
+      {/* <div>User name is {user?.name}</div>
+      <div>User email is {user?.email}</div> */}
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
     </div>
   );
 };
