952929b76d03b9cbe323710f57b19865a7de37ac
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2023-07-25 03:08:13 +0700

    Template Literals and Exclude

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { RandomNumber } from "./components/restriction/RandomNumber";
+import { Toast } from "./components/templateliterals/Toast";
+// import { RandomNumber } from "./components/restriction/RandomNumber";
 // import { Private } from "./components/auth/Private";
 // import { Profile } from "./components/auth/Profile";
 // import { List } from "./components/generics/List";
@@ -99,7 +100,8 @@ function App() {
         ]}
         onClick={(item) => console.log(item)}
       /> */}
-      <RandomNumber value={10} isPositive />
+      {/* <RandomNumber value={10} isPositive /> */}
+      <Toast position="center" />
     </div>
   );
 }
---
+++ src/components/templateliterals/Toast.tsx
@@ -0,0 +1,15 @@
+// Position prop can be one off
+// "left-center" | "left-top" | "left-bottom" | "center" | "center-top" | "center-bottom" | "right-center" | "right-top" | "right-bottom"
+
+type HorizontalPosition = "left" | "center" | "right";
+type VerticalPosition = "top" | "center" | "bottom";
+
+type ToastProps = {
+  position:
+    | Exclude<`${HorizontalPosition}-${VerticalPosition}`, "center-center">
+    | "center";
+};
+
+export const Toast = ({ position }: ToastProps) => {
+  return <div>Toast Notification Position - {position}</div>;
+};
